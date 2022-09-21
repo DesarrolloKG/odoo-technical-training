d100d352cc85b2e6b6384d1ec347f891be7948cd
Rodolfo <rodolfo.cervantes@kurigage.com>
2022-09-21 20:35:13 +0000

    Modulo basico

---
+++ odoo_academy/__manifest__.py
@@ -0,0 +1,18 @@
+{
+    'name': 'Odoo Academy ADN',
+    'summary': """Academy app to manage training""",
+    'description': """
+        Academy module to manage training
+    """,
+    'author': 'El Adan',
+    'website': 'erp.appskurigage.com',
+    'category': 'Training',
+    'version': '0.1',
+    'depends': ['base'],
+    'data': [
+        
+    ],
+    'demo':[
+        
+    ],
+}
