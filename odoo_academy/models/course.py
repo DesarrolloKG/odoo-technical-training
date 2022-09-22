d5bec4c16e5584a553040cb410ea346f2e795c6a
Arnulfo Casique <arnulfo.nolasco@kurigage.com>
2022-09-22 10:17:48 -0500

    model

--- odoo_academy/models/course.py
+++ odoo_academy/models/course.py
@@ -1,5 +1,6 @@
 # -*- coding: utf-8 -*-
 
+from email.policy import default
 from odoo import models, fields, api
 
 class Course(models.Model):
@@ -7,3 +8,14 @@ class Course(models.Model):
     _name = 'academy.course'
     _description = 'Course info' 
 
+    name = fields.Char(string = 'Title', required = true)
+    description = fields.Text(string = 'Description')
+
+    level = fields.Selection(string = 'Leven', selection = [
+        ('beginner', 'Beginner'), 
+        ('intermediate', 'Intermediate'),
+        ('advanced', 'Advanced')
+    ], copy = False)
+
+    active = fields.Boolean(string = 'Active', default = True)
+    
