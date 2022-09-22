6ad2eac2d49e8ee550b1ca7eb1840f6cf9aaf11d
Arnulfo Casique <arnulfo.nolasco@kurigage.com>
2022-09-22 12:06:59 -0500

    model

--- odoo_academy/models/course.py
+++ odoo_academy/models/course.py
@@ -11,7 +11,7 @@ class Course(models.Model):
     name = fields.Char(string = 'Title', required = True)
     description = fields.Text(string = 'Description')
 
-    level = fields.Selection(string = 'Leven', selection = [
+    level = fields.Selection(string = 'Level', selection = [
         ('beginner', 'Beginner'), 
         ('intermediate', 'Intermediate'),
         ('advanced', 'Advanced')
