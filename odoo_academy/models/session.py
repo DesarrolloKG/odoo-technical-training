a4e406a2ab817188bf961d697918cdf529bdd3ed
Arnulfo Casique <arnulfo.nolasco@kurigage.com>
2022-09-27 14:56:07 -0500

    views

--- odoo_academy/models/session.py
+++ odoo_academy/models/session.py
@@ -22,7 +22,7 @@ class Session(models.Model):
                 record.end_date = record.start_date
             else:
                 duration = timedelta(days=record.duration)
-                record.end_date = record.start_data + duration
+                record.end_date = record.start_date + duration
 
     def _inverse_end_date(self):
         for record in self:
