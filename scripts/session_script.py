a77b0d0b48b027d66d4b6b2c7194e911bc91476e
Arnulfo Casique <arnulfo.nolasco@kurigage.com>
2022-09-30 14:05:49 -0500

    api

--- scripts/session_script.py
+++ scripts/session_script.py
@@ -13,11 +13,5 @@ print(uid)
 
 models = client.ServerProxy("{}/xmlrpc/2/object".format(url))
 
-model_access = models.execute_kw(db, uid, password, 'sale.order', 'check_access_rights', ['write'], {'raise_exception': False})
+model_access = models.execute_kw(db, uid, password, 'academy.session', 'check_access_rights', ['write'], {'raise_exception': False})
 print(model_access)
-
-draft_quotes = models.execute_kw(db, uid, password, 'sale.order', 'search', [[['state', '=', 'draft']]])
-print(draft_quotes)
-
-if_confirmed = models.execute_kw(db, uid, password, 'sale.order', 'action_confirm', [draft_quotes])
-print(if_confirmed)
