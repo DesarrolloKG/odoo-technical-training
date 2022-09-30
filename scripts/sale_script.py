54c46b38ca687a1df8cbbecd93f6bc44abed9489
Arnulfo Casique <arnulfo.nolasco@kurigage.com>
2022-09-30 12:39:57 -0500

    api

---
+++ scripts/sale_script.py
@@ -0,0 +1,9 @@
+from xmlrpc import client
+
+url = "https://desarrollokg-odoo-technical-training2-main-5991710.dev.odoo.com/"
+db = "desarrollokg-odoo-technical-training2-main-5991710"
+username = "admin"
+password = "admin"
+
+common = client.ServerProxy("{}/xmlrpc/2/common".format(url))
+print(common.version())
