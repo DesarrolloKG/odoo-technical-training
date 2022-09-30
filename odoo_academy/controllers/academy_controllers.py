9e1f9ccadcb28f55b62933298295974b441c1eaa
Rodolfo <rodolfo.cervantes@kurigage.com>
2022-09-30 21:48:56 +0000

    Controller 1

--- odoo_academy/controllers/academy_controllers.py
+++ odoo_academy/controllers/academy_controllers.py
@@ -8,12 +8,12 @@ class Academy(http.Controller):
     @http.route('/academy/courses/', auth='public', website=True)
     def courses(self, **kw):
         courses = http.request.env['academy.course'].search([])
-        return http.request.render('odoo.academy.course_website', {
+        return http.request.render('odoo_academy.course_website', {
             'courses': courses,
         })
     
     @http.route('/academy/<model("academy.session"):session>/', auth='public', website=True)
     def session(self, session):
-        return http.request.render('odoo.academy.session_website',{
+        return http.request.render('odoo_academy.session_website',{
             'session': session,
         })
