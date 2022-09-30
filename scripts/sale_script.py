c19c2c6fec583c93938f5647b8b367b8b2139a37
Arnulfo Casique <arnulfo.nolasco@kurigage.com>
2022-09-30 13:18:50 -0500

    api

--- scripts/sale_script.py
+++ scripts/sale_script.py
@@ -15,3 +15,6 @@ models = client.ServerProxy("{}/xmlrpc/2/object".format(url))
 
 model_access = models.execute_kw(db, uid, password, 'sale.order', 'check_access_rights', ['write'], {'raise_exception': False})
 print(model_access)
+
+draft_quotes = models.execute_kw(db, uid, password, 'sale.order', 'search', [[['state', '=', 'draft']]])
+print(draft_quotes)
